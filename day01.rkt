2fcdff0a6a1734d0cfa4add43a87b36da30fb7b8
liutaurasvilda <liutauras.vilda@liveramp.com>
2022-12-01 12:57:39 +0000

    day01 done

--- day01.rkt
+++ day01.rkt
@@ -1,3 +1,13 @@
 #lang racket
 
-(define input (file->lines "input/day01.txt"))
+(define input (map (λ (e) (string->number e)) (file->lines "input/day01.txt")))
+
+(define (calories-calc calories sum calories-bag)
+  (cond [(empty? calories) (cons sum calories-bag)]
+        [(not (car calories)) (calories-calc (cdr calories) 0 (cons sum calories-bag))]
+        [else (calories-calc (cdr calories) (+ sum (car calories)) calories-bag)]))
+
+(define sorted-calories (sort (calories-calc input 0 '()) >))
+
+(car sorted-calories)
+(apply + (take sorted-calories 3))

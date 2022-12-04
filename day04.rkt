cd5ae580c5739a11b872b2a3ce89ad204e28ae79
liutaurasvilda <liutauras.vilda@gmail.com>
2022-12-04 11:23:07 +0000

    day04 done

--- day04.rkt
+++ day04.rkt
@@ -1,3 +1,27 @@
 #lang racket
 
-(define input (file->lines "input/day04.txt"))
+(define input
+  (map (λ (e) (list (inclusive-range (first (car e)) (second (car e))) (inclusive-range (first (cadr e)) (second (cadr e)))))
+       (map (λ (e) (list (map (λ (f) (string->number f)) (car e)) (map (λ (f) (string->number f)) (cadr e))))
+            (map (λ (e) (list (string-split (car e) "-") (string-split (cadr e) "-")))
+                 (map (λ (e) (string-split e ",")) (file->lines "input/day04.txt"))))))
+
+(define (play input sum)
+  (cond [(empty? input) sum]
+        [(< (car (car (car input))) (car (cadr (car input))))
+         (if (= (length (set-intersect (car (car input)) (cadr (car input)))) (length (cadr (car input)))) (play (cdr input) (add1 sum)) (play (cdr input) sum))]
+        [(> (car (car (car input))) (car (cadr (car input))))
+         (if (= (length (set-intersect (car (car input)) (cadr (car input)))) (length (car (car input)))) (play (cdr input) (add1 sum)) (play (cdr input) sum))]
+        [(and (= (car (car (car input))) (car (cadr (car input)))) (= (last (car (car input))) (last (cadr (car input))))) (play (cdr input) (add1 sum))]
+        [(= (car (car (car input))) (car (cadr (car input))))
+         (cond [(< (last (car (car input))) (last (cadr (car input))))
+                (if (= (length (set-intersect (car (car input)) (cadr (car input)))) (length (car (car input)))) (play (cdr input) (add1 sum)) (play (cdr input) sum))]
+               [else (if (= (length (set-intersect (car (car input)) (cadr (car input)))) (length (cadr (car input)))) (play (cdr input) (add1 sum)) (play (cdr input) sum))])]))
+
+(define (play2 input sum)
+  (cond [(empty? input) sum]
+        [(not (empty? (set-intersect (car (car input)) (cadr (car input))))) (play2 (cdr input) (add1 sum))]
+        [else (play2 (cdr input) sum)]))
+
+(play input 0)
+(play2 input 0)

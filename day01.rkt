740ef4815b50afabe40b27bab2f8a81d1353764a
liutaurasvilda <liutauras.vilda@gmail.com>
2022-12-04 11:22:30 +0000

    day01 refactor

--- day01.rkt
+++ day01.rkt
@@ -1,15 +1,11 @@
 #lang racket
 
-(define input
-  (map (λ (e) (string->number e))
-       (file->lines "input/day01.txt")))
+(define input (map (λ (e) (string->number e)) (file->lines "input/day01.txt")))
 
 (define (calories-calc calories sum calories-bag)
-  (cond [(empty? calories) (cons sum calories-bag)]
+  (cond [(empty? calories) (sort (cons sum calories-bag) >)]
         [(not (car calories)) (calories-calc (cdr calories) 0 (cons sum calories-bag))]
         [else (calories-calc (cdr calories) (+ sum (car calories)) calories-bag)]))
 
-(define sorted-calories (sort (calories-calc input 0 '()) >))
-
-(car sorted-calories)
-(apply + (take sorted-calories 3))
+(car (calories-calc input 0 '()))
+(apply + (take (calories-calc input 0 '()) 3))

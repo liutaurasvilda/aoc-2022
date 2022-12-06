b1bb2f515c84c74d29afdd8244668a64749fbc5c
liutaurasvilda <liutauras.vilda@gmail.com>
2022-12-06 10:38:40 +0000

    day06 done

--- day06.rkt
+++ day06.rkt
@@ -1,3 +1,10 @@
 #lang racket
 
-(define input (file->lines "input/day06.txt"))
+(define input (string->list (car (file->lines "input/day06.txt"))))
+
+(define (play input slice count)
+  (cond [(not (check-duplicates (take input slice))) count]
+        [else (play (cdr input) slice (add1 count))]))
+
+(play input 4 4)   ; 1760
+(play input 14 14) ; 2974

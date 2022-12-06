bf74927211765f47d0345a82cf0e6af13ec994bb
liutaurasvilda <liutauras.vilda@gmail.com>
2022-12-06 17:25:27 +0000

    rename

--- day06.rkt
+++ day06.rkt
@@ -3,7 +3,7 @@
 (define input (string->list (car (file->lines "input/day06.txt"))))
 
 (define (solve input slice count)
-  (if (not (check-duplicates (take input slice))) (+ count slice)
+  (if (not (check-duplicates (take input slice))) (+ slice count)
       (solve (cdr input) slice (add1 count))))
 
 (solve input 4 0)

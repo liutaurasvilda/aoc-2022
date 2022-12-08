6884b9d7c795f5d7a82f96214247057ef677adec
liutaurasvilda <liutauras.vilda@liveramp.com>
2022-12-08 10:02:47 +0000

    day08 some start

--- day08.rkt
+++ day08.rkt
@@ -1,3 +1,12 @@
 #lang racket
 
-(define input (file->lines "input/day08.txt"))
+(define input (map (λ (e) (map (λ (f) (string->number f)) (map string (string->list e))))
+                   (file->lines "input/day08_test.txt")))
+
+(define (solve input)
+  (for* ([i (in-range (length input))]
+         [j (in-range (length (car input)))])
+    (let ([tree (list-ref (list-ref input i) j)])
+          (displayln tree))))
+
+(solve input)

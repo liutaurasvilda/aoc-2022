9f96ed108ed4492f2f7fd636fad3439b00cce62f
liutaurasvilda <liutauras.vilda@gmail.com>
2022-12-10 12:15:49 +0000

    day09 refactor

--- day09.rkt
+++ day09.rkt
@@ -1,9 +1,20 @@
 #lang racket
+(require rackunit)
 
 (define input
   (map (λ (e) (list (car e) (string->number (cadr e))))
        (map (λ (e) (string-split e " "))
             (file->lines "input/day09.txt"))))
+
+(define test-input
+  (map (λ (e) (list (car e) (string->number (cadr e))))
+       (map (λ (e) (string-split e " "))
+            (file->lines "input/day09_test.txt"))))
+
+(define test-input-2
+  (map (λ (e) (list (car e) (string->number (cadr e))))
+       (map (λ (e) (string-split e " "))
+            (file->lines "input/day09_test2.txt"))))
 
 (define (need-move? head tail)
   (or (> (abs (- (first head) (first tail))) 1)
@@ -14,26 +25,28 @@
          [head-j (second head)]
          [tail-i (first tail)]
          [tail-j (second tail)])
-    (cond [(equal? direction "U")
-           (cond
-             [(< tail-j head-j) (list (- tail-i 1) (+ tail-j 1))]
-             [(= tail-j head-j) (list (- tail-i 1) tail-j)]
-             [(> tail-j head-j) (list (- tail-i 1) (- tail-j 1))])]
-          [(equal? direction "D")
-           (cond
-             [(< tail-j head-j) (list (+ tail-i 1) (+ tail-j 1))]
-             [(= tail-j head-j) (list (+ tail-i 1) tail-j)]
-             [(> tail-j head-j) (list (+ tail-i 1) (- tail-j 1))])]
-          [(equal? direction "L")
-           (cond
-             [(< tail-i head-i) (list (+ tail-i 1) (- tail-j 1))]
-             [(= tail-i head-i) (list tail-i (- tail-j 1))]
-             [(> tail-i head-i) (list (- tail-i 1) (- tail-j 1))])]
-          [(equal? direction "R")
-           (cond
-             [(< tail-i head-i) (list (+ tail-i 1) (+ tail-j 1))]
-             [(= tail-i head-i) (list tail-i (+ tail-j 1))]
-             [(> tail-i head-i) (list (- tail-i 1) (+ tail-j 1))])])))
+    (cond [(need-move? head tail)
+           (cond [(equal? direction "U")
+                  (cond
+                    [(< tail-j head-j) (list (- tail-i 1) (+ tail-j 1))]
+                    [(= tail-j head-j) (list (- tail-i 1) tail-j)]
+                    [(> tail-j head-j) (list (- tail-i 1) (- tail-j 1))])]
+                 [(equal? direction "D")
+                  (cond
+                    [(< tail-j head-j) (list (+ tail-i 1) (+ tail-j 1))]
+                    [(= tail-j head-j) (list (+ tail-i 1) tail-j)]
+                    [(> tail-j head-j) (list (+ tail-i 1) (- tail-j 1))])]
+                 [(equal? direction "L")
+                  (cond
+                    [(< tail-i head-i) (list (+ tail-i 1) (- tail-j 1))]
+                    [(= tail-i head-i) (list tail-i (- tail-j 1))]
+                    [(> tail-i head-i) (list (- tail-i 1) (- tail-j 1))])]
+                 [(equal? direction "R")
+                  (cond
+                    [(< tail-i head-i) (list (+ tail-i 1) (+ tail-j 1))]
+                    [(= tail-i head-i) (list tail-i (+ tail-j 1))]
+                    [(> tail-i head-i) (list (- tail-i 1) (+ tail-j 1))])])]
+          [else tail])))
 
 (define (track-tail head tail-path direction)
   (let* ([head-i (first head)]
@@ -44,8 +57,12 @@
     (cond [(need-move? head tail) (track-tail head (cons (move-tail head tail direction) tail-path) direction)]
           [else tail-path])))
 
-(define (move-body head body result direction)
-  '((0 0))) 
+(define (move-body head body new-body direction)
+  (cond [(empty? body) (reverse new-body)]
+        [else
+         (let* ([knot (first (track-tail head body direction))]
+                [body-result (cons knot new-body)])
+           (move-body knot (cdr body) body-result direction))]))
 
 (define (move-head head direction distance)
   (cond [(> distance 0)
@@ -70,7 +87,16 @@
          (let* ([new-head (move-head head (first (car input)) (second (car input)))]
                 [new-body (move-body new-head body '() (first (car input)))]
                 [new-tail-path (track-tail (last new-body) tail-path (first (car input)))])
+           (display "instruction ") (displayln (car input))
+           (display "head ") (displayln new-head)
+           (display "body ") (displayln new-body)
+           (display "tail ") (displayln (first new-tail-path))
+           (displayln "")
            (solve2 (cdr input) new-head new-body new-tail-path))]))
 
-(solve1 input '(0 0) '((0 0)))
-(solve2 input '(0 0) '((0 0) (0 0) (0 0) (0 0) (0 0) (0 0) (0 0) (0 0)) '((0 0)))
+
+; unit tests
+(check-equal? (solve1 test-input '(0 0) '((0 0))) 13 "part1 input/day09_test.txt")
+(check-equal? (solve1 input '(0 0) '((0 0))) 6175 "part1 input/day09.txt")
+(check-equal? (solve2 test-input '(0 0) '((0 0) (0 0) (0 0) (0 0) (0 0) (0 0) (0 0) (0 0)) '((0 0))) 1 "part2 input/day09_test.txt")
+;(check-equal? (solve2 test-input-2 '(0 0) '((0 0) (0 0) (0 0) (0 0) (0 0) (0 0) (0 0) (0 0)) '((0 0))) 36 "part2 input/day09_test2.txt")

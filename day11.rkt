fd753b91f34e5a02a79047abc2fc36d1e535b305
liutaurasvilda <liutauras.vilda@gmail.com>
2022-12-12 21:16:57 +0000

    day11 part2 sick

--- day11.rkt
+++ day11.rkt
@@ -2,83 +2,84 @@
 
 (struct monkey (name [items #:mutable] operation throw-to) #:transparent)
 
-#|
+(define worry-reducer 1)
+
 (define monkey-0
   (monkey
    "0"
    '(79 98)
-   (λ (e) (floor (/ (* e 19) 3)))
+   (λ (e) (floor (/ (* e 19) worry-reducer)))
    (λ (e) (if (= (modulo e 23) 0) 2 3))))
 
 (define monkey-1
   (monkey
    "1"
    '(54 65 75 74)
-   (λ (e) (floor (/ (+ e 6) 3)))
+   (λ (e) (floor (/ (+ e 6) worry-reducer)))
    (λ (e) (if (= (modulo e 19) 0) 2 0))))
 
 (define monkey-2
   (monkey
    "2"
    '(79 60 97)
-   (λ (e) (floor (/ (* e e) 3)))
+   (λ (e) (floor (/ (* e e) worry-reducer)))
    (λ (e) (if (= (modulo e 13) 0) 1 3))))
 
 (define monkey-3
   (monkey
    "3"
    '(74)
-   (λ (e) (floor (/ (+ e 3) 3)))
+   (λ (e) (floor (/ (+ e 3) worry-reducer)))
    (λ (e) (if (= (modulo e 17) 0) 0 1))))
-|#
 
+#| REAL INPUT
 (define monkey-0
   (monkey
    "0"
    '(54 82 90 88 86 54)
-   (λ (e) (floor (/ (* e 7) 3)))
+   (λ (e) (floor (/ (* e 7) worry-reducer)))
    (λ (e) (if (= (modulo e 11) 0) 2 6))))
 
 (define monkey-1
   (monkey
    "1"
    '(91 65)
-   (λ (e) (floor (/ (* e 13) 3)))
+   (λ (e) (floor (/ (* e 13) worry-reducer)))
    (λ (e) (if (= (modulo e 5) 0) 7 4))))
 
 (define monkey-2
   (monkey
    "2"
    '(62 54 57 92 83 63 63)
-   (λ (e) (floor (/ (+ e 1) 3)))
+   (λ (e) (floor (/ (+ e 1) worry-reducer)))
    (λ (e) (if (= (modulo e 7) 0) 1 7))))
 
 (define monkey-3
   (monkey
    "3"
    '(67 72 68)
-   (λ (e) (floor (/ (* e e) 3)))
+   (λ (e) (floor (/ (* e e) worry-reducer)))
    (λ (e) (if (= (modulo e 2) 0) 0 6))))
 
 (define monkey-4
   (monkey
    "4"
    '(68 89 90 86 84 57 72 84)
-   (λ (e) (floor (/ (+ e 7) 3)))
+   (λ (e) (floor (/ (+ e 7) worry-reducer)))
    (λ (e) (if (= (modulo e 17) 0) 3 5))))
 
 (define monkey-5
   (monkey
    "5"
    '(79 83 64 58)
-   (λ (e) (floor (/ (+ e 6) 3)))
+   (λ (e) (floor (/ (+ e 6) worry-reducer)))
    (λ (e) (if (= (modulo e 13) 0) 3 0))))
 
 (define monkey-6
   (monkey
    "6"
    '(96 72 89 70 88)
-   (λ (e) (floor (/ (+ e 4) 3)))
+   (λ (e) (floor (/ (+ e 4) worry-reducer)))
    (λ (e) (if (= (modulo e 3) 0) 1 2))))
 
 (define monkey-7
@@ -87,18 +88,20 @@
    '(79)
    (λ (e) (floor (/ (+ e 8) 3)))
    (λ (e) (if (= (modulo e 19) 0) 4 5))))
+|#
 
 (define h (make-hash))
 (hash-set! h "0" 0)
 (hash-set! h "1" 0)
 (hash-set! h "2" 0)
 (hash-set! h "3" 0)
-(hash-set! h "4" 0)
-(hash-set! h "5" 0)
-(hash-set! h "6" 0)
-(hash-set! h "7" 0)
+;(hash-set! h "4" 0)
+;(hash-set! h "5" 0)
+;(hash-set! h "6" 0)
+;(hash-set! h "7" 0)
 
-(define monkeys (list monkey-0 monkey-1 monkey-2 monkey-3 monkey-4 monkey-5 monkey-6 monkey-7))
+;(define monkeys (list monkey-0 monkey-1 monkey-2 monkey-3 monkey-4 monkey-5 monkey-6 monkey-7))
+(define monkeys (list monkey-0 monkey-1 monkey-2 monkey-3))
 
 (define (increase-inspected monkey)
   (let* ([monkey-name (monkey-name monkey)]
@@ -129,6 +132,7 @@
          (cond [(empty? queue) (solve monkeys monkeys (add1 round) required-rounds)]
                [else
                 (inspect (car queue) monkeys)
+                (displayln "")
                 (solve (cdr queue) monkeys round required-rounds)])]))
 
-(solve monkeys monkeys 1 20)
+(solve monkeys monkeys 1 1)

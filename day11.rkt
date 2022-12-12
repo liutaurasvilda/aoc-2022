e9227bac7bc968ce2aef4d2ef7c91c407eef6c75
liutaurasvilda <liutauras.vilda@gmail.com>
2022-12-12 01:45:36 +0000

    day11 progress

--- day11.rkt
+++ day11.rkt
@@ -26,28 +26,28 @@
   (monkey
    "0"
    '(79 98)
-   (λ (e) (inexact->exact (/ (* e 19) 3)))
+   (λ (e) (round (/ (* e 19) 3)))
    (λ (e) (if (= (modulo e 23) 0) 2 3))))
 
 (define monkey-1
   (monkey
    "1"
    '(54 65 75 74)
-   (λ (e) (inexact->exact (/ (+ e 6) 3)))
+   (λ (e) (round (/ (+ e 6) 3)))
    (λ (e) (if (= (modulo e 19) 0) 2 0))))
 
 (define monkey-2
   (monkey
    "2"
    '(79 60 97)
-   (λ (e) (inexact->exact (/ (* e e) 3)))
+   (λ (e) (round (/ (* e e) 3)))
    (λ (e) (if (= (modulo e 13) 0) 1 3))))
 
 (define monkey-3
   (monkey
    "3"
    '(74)
-   (λ (e) (inexact->exact (/ (+ e 3) 3)))
+   (λ (e) (round (/ (+ e 3) 3)))
    (λ (e) (if (= (modulo e 17) 0) 0 1))))
 
 (define (inspect monkey monkeys)
@@ -55,7 +55,7 @@
         [operation-f (monkey-operation monkey)]
         [throw-to-f (monkey-throw-to monkey)])
     (for ([i items])
-      (display i))
+      (throw-item monkey (list-ref monkeys (throw-to-f (operation-f i)))))))
 
 (define (solve queue monkeys round required-rounds)
   (cond [(> round required-rounds) (apply * (take (sort (hash-values h) >) 2))]

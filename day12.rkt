81c676f1a401faee811fad30ca3728aed9181145
liutaurasvilda <liutauras.vilda@gmail.com>
2022-12-13 23:36:35 +0000

    day12 input prep

--- day12.rkt
+++ day12.rkt
@@ -1,7 +1,10 @@
 #lang racket
 
+(define to-number
+  (λ (e) (if (char-upper-case? e) 0 (- (char->integer e) 96))))
+
 (define input
-  (map (λ (e) (string->list e))
+  (map (λ (e) (map to-number (string->list e)))
        (file->lines "input/day12_test.txt")))
 
 (define (solve heightmap location visited paths)

c0cbf36cb706cb578969d5bb5f21bb154936e739
liutaurasvilda <liutauras.vilda@gmail.com>
2022-12-14 01:10:46 +0000

    day12 some progress

--- day12.rkt
+++ day12.rkt
@@ -3,24 +3,30 @@
 (define to-number
   (λ (e) (if (char-upper-case? e) 0 (- (char->integer e) 96))))
 
-(define heightmap
+(define input
   (map (λ (e) (map to-number (string->list e)))
        (file->lines "input/day12_test.txt")))
 
-(define up (λ (e) (list (- (first e) 1) (second e))))
-(define down (λ (e) (list (+ (first e) 1) (second e))))
-(define left (λ (e) (list (first e) (- (second e) 1))))
-(define right (λ (e) (list (first e) (+ (second e) 1))))
+(define location->up (λ (e) (list (- (first e) 1) (second e))))
+(define location->down (λ (e) (list (+ (first e) 1) (second e))))
+(define location->left (λ (e) (list (first e) (- (second e) 1))))
+(define location->right (λ (e) (list (first e) (+ (second e) 1))))
 
 (define (element-at location)
   (let ([row (first location)]
         [column (second location)])
-    (cond [(and (row > -1) (< row (length heightmap)) (> column -1) (< column (length (list-ref heightmap 0))))
-           (list-ref (list-ref heightmap row) column)]
+    (cond [(and (> row -1) (< row (length input)) (> column -1) (< column (length (list-ref input 0))))
+           (list-ref (list-ref input row) column)]
           [else #f])))
 
-(define (allowed-from? location direction)
-  (displayln "not implemented"))
+(define (allowed-from? location direction visited)
+  (let* ([next-location (direction location)]
+        [current-element (element-at location)]
+        [next-element (element-at next-location)])
+    (cond [(set-member? visited next-location) #f]
+          [(equal? #f next-element) #f]
+          [(equal? next-element 0) #t]
+          [else (= (add1 current-element) next-element) #t])))
 
 (define (solve location visited paths)
   (displayln "not implemented"))

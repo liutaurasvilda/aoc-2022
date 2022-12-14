5c797c3025ddb0c32a43c4a8068da2dee8cb31d9
liutaurasvilda <liutauras.vilda@gmail.com>
2022-12-14 18:06:07 +0000

    refactor

--- day12.rkt
+++ day12.rkt
@@ -3,11 +3,9 @@
 (require dyoo-while-loop)
 
 (define to-number
-  (λ (e)
-    (if
-     (char-upper-case? e)
-     (if (equal? e #\S) 1 26)
-     (- (char->integer e) 96))))
+  (λ (e) (if (char-upper-case? e)
+             (if (equal? e #\S) 1 26)
+             (- (char->integer e) 96))))
 
 (define input
   (map (λ (e) (map to-number (string->list e)))
@@ -18,8 +16,6 @@
 (define location->left (λ (e) (list (first e) (- (second e) 1))))
 (define location->right (λ (e) (list (first e) (+ (second e) 1))))
 
-(define visited (mutable-set))
-
 (define (element-at location)
   (let ([row (first location)]
         [column (second location)])
@@ -27,32 +23,35 @@
            (list-ref (list-ref input row) column)]
           [else #f])))
 
-(define (visitable? source-location target-location)
+(define (visitable? source-location target-location visited)
   (let* ([source-element (element-at source-location)]
          [target-element (element-at target-location)])
     (cond [(or (equal? #f target-element) (set-member? visited target-location)) #f]
           [(< (add1 source-element) target-element) #f]
           [else #t])))
 
-(define (visitable-neighbours-of location)
-  (filter (λ (e) (visitable? location e))
+(define (visitable-neighbours-of location visited)
+  (filter (λ (e) (visitable? location e visited))
           (list (location->up location) (location->down location)
                 (location->left location) (location->right location))))
 
-(define (solve target q steps)
-  (cond [(non-empty-queue? q)
-         (let* ([location (dequeue! q)]
-                [neighbours (visitable-neighbours-of location)])
+(define (bfs target queue visited steps)
+  (cond [(non-empty-queue? queue)
+         (let* ([location (dequeue! queue)]
+                [neighbours (visitable-neighbours-of location visited)])
            (cond [(member target neighbours) steps]
                  [else
                   (for ([i neighbours])
-                    (enqueue! q i)
+                    (enqueue! queue i)
                     (set-add! visited i))
-                  (solve target q (add1 steps))]))]
-        [else 0]))
+                  (bfs target queue visited (add1 steps))]))]
+        [else #f]))
 
-(define q (make-queue))
-(enqueue! q '(0 0))
-(set-add! visited '(0 0))
+(define (solve start target)
+  (let ([queue (make-queue)]
+        [visited (mutable-set)])
+    (enqueue! queue start)
+    (set-add! visited start)
+    (bfs target queue visited 1)))
 
-(solve '(2 5) q 1)
+(solve '(0 0) '(2 5))

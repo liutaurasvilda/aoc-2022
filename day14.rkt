1e691ff6c4f6c50be5a95286afc9ff5ee7f3dff0
liutaurasvilda <liutauras.vilda@gmail.com>
2022-12-16 20:40:11 +0000

    day14 parse input

--- day14.rkt
+++ day14.rkt
@@ -1,3 +1,10 @@
 #lang racket
 
-(define input (file->lines "input/day14_test.txt"))
+(define to-number
+  (λ (e)
+    (let ([pair (string-split e ",")])
+      (list (string->number (car pair)) (string->number (cadr pair))))))
+
+(define input
+  (map (λ (e) (map to-number (string-split e " -> ")))
+       (file->lines "input/day14_test.txt")))

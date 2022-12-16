04e622147678c3cb2dd596a0066df21f45175032
liutaurasvilda <liutauras.vilda@liveramp.com>
2022-12-16 16:49:41 +0000

    day13 part1 done

--- day13.rkt
+++ day13.rkt
@@ -3,10 +3,13 @@
 
 (define input
   (map (λ (e) (string->list e))
-       (file->lines "input/day13_test.txt")))
+       (file->lines "input/day13.txt")))
 
 (define (c c)
   (char->integer c))
+
+(define (number? c)
+  (pair? (member (char->integer c) (sequence->list (in-inclusive-range 48 58)))))
 
 (define (standardize packet)
   (cons #\[ (cons (car packet) (cons #\] (cdr packet)))))
@@ -14,14 +17,14 @@
 (define (in-order? packet1 packet2)
   (let ([e1 (car packet1)]
         [e2 (car packet2)])
-    (cond [(and (equal? e1 #\]) (char-numeric? e2)) #t]
-          [(and (char-numeric? e1) (equal? e2 #\])) #f]
-          [(and (equal? e1 #\[) (equal? e2 #\])) #f]
-          [(and (equal? e1 #\]) (equal? e2 #\[)) #t]
-          [(and (char-numeric? e1) (not (char-numeric? e2))) (in-order? (standardize packet1) packet2)]
-          [(and (not (char-numeric? e1)) (char-numeric? e2)) (in-order? packet1 (standardize packet2))]
+    (cond [(and (equal? e1 #\]) (number? e2)) #t]
+          [(and (number? e1) (equal? e2 #\])) #f]
           [(and (equal? e1 #\]) (equal? e2 #\,)) #t]
           [(and (equal? e1 #\,) (equal? e2 #\])) #f]
+          [(and (equal? e1 #\[) (equal? e2 #\])) #f]
+          [(and (equal? e1 #\]) (equal? e2 #\[)) #t]
+          [(and (number? e1) (not (number? e2))) (in-order? (standardize packet1) packet2)]
+          [(and (not (number? e1)) (number? e2)) (in-order? packet1 (standardize packet2))]
           [(> (c e2) (c e1)) #t]
           [(< (c e2) (c e1)) #f]
           [(= (c e1) (c e2)) (in-order? (cdr packet1) (cdr packet2))])))
@@ -31,6 +34,9 @@
         [(in-order? (car input) (cadr input)) (solve (cddr input) (add1 index) (cons index indices))]
         [else (solve (cddr input) (add1 index) indices)]))
 
+(solve input 1 '())
+
+#|
 (check-equal?
  (in-order?
   '(#\[ #\1 #\, #\1 #\, #\3 #\, #\1 #\, #\1 #\])
@@ -78,3 +84,4 @@
   '(#\[ #\1 #\, #\[ #\2 #\, #\[ #\3 #\, #\[ #\4 #\, #\[ #\5 #\, #\6 #\, #\7 #\] #\] #\] #\] #\, #\8 #\, #\9 #\])
   '(#\[ #\1 #\, #\[ #\2 #\, #\[ #\3 #\, #\[ #\4 #\, #\[ #\5 #\, #\6 #\, #\0 #\] #\] #\] #\] #\, #\8 #\, #\9 #\]))
  #f "pair 8")
+|#

1d591e46ee9face4e350a3ec861c84a387f650df
liutaurasvilda <liutauras.vilda@gmail.com>
2022-12-23 22:17:49 +0000

    day18 refactor

--- day18.rkt
+++ day18.rkt
@@ -11,21 +11,22 @@
 (define (count-covered-sides input total-sides)
   (cond [(empty? input) total-sides]
         [else
-  (let* ([cube (car input)]
-         [remaining-cubes (cdr input)]
-         [x-diff-1 (list (- (first cube) 1) (second cube) (third cube))]
-         [x-diff-2 (list (+ (first cube) 1) (second cube) (third cube))]
-         [y-diff-1 (list (first cube) (- (second cube) 1) (third cube))]
-         [y-diff-2 (list (first cube) (+ (second cube) 1) (third cube))]
-         [z-diff-1 (list (first cube) (second cube) (- (third cube) 1))]
-         [z-diff-2 (list (first cube) (second cube) (+ (third cube) 1))]
-         [covered-sides (list
-                         (covers? x-diff-1 remaining-cubes)
-                         (covers? x-diff-2 remaining-cubes)
-                         (covers? y-diff-1 remaining-cubes)
-                         (covers? y-diff-2 remaining-cubes)
-                         (covers? z-diff-1 remaining-cubes)
-                         (covers? z-diff-2 remaining-cubes))])
-    (count-covered-sides (cdr input) (- total-sides (* (count positive? covered-sides) 2))))]))
+         (let* ([cube (car input)]
+                [remaining-cubes (cdr input)]
+                [x-diff-1 (list (- (first cube) 1) (second cube) (third cube))]
+                [x-diff-2 (list (+ (first cube) 1) (second cube) (third cube))]
+                [y-diff-1 (list (first cube) (- (second cube) 1) (third cube))]
+                [y-diff-2 (list (first cube) (+ (second cube) 1) (third cube))]
+                [z-diff-1 (list (first cube) (second cube) (- (third cube) 1))]
+                [z-diff-2 (list (first cube) (second cube) (+ (third cube) 1))]
+                [covered-sides (* (count positive?
+                                         (list
+                                          (covers? x-diff-1 remaining-cubes)
+                                          (covers? x-diff-2 remaining-cubes)
+                                          (covers? y-diff-1 remaining-cubes)
+                                          (covers? y-diff-2 remaining-cubes)
+                                          (covers? z-diff-1 remaining-cubes)
+                                          (covers? z-diff-2 remaining-cubes))) 2)])
+           (count-covered-sides (cdr input) (- total-sides covered-sides)))]))
 
 (count-covered-sides input (* (length input) 6))
